723bb1198cb67ff2e52d0d9ba559fb398567d762
strawHat <871182024@qq.com>
2018-04-10 09:46:47 +0800

    update code to remote github

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("the second state of studying git!");
+		System.out.println("update code to remote github");
 	}
 
 }
